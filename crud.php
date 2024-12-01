fa7d49c34f55033270f48265b92842213bc3aeae
emma-boamah <emmanuelboamah046@gmail.com>
2024-11-30 16:24:40 -0800

    Enhanced user validation

--- crud.php
+++ crud.php
@@ -8,14 +8,14 @@
 <body>
     <h2><div>INSERT DATA FORM</div></h2>
     <div>
-        <form action="connect.php" method="POST">
+        <form action="<?php echo $_SERVER["PHP_SELF"]?>" method="POST">
             <div>
                 <label for="form_Number">Form Number</label>
                 <input type="text" name="form_Number" value="" placeholder="Enter Form ID">
             </div>
             <div>
                 <label for="full_Name">Full Name</label>
-                <input type="text" name="full_Name" value="" placeholder="Enter Full Name">
+                <input type="text" name="full_Name" value="" placeholder="Enter Full Name" required>
             </div>
             <div>
                 <label for="home_Address">Home Address</label>
@@ -43,62 +43,71 @@
 </html>
 <!-- PHP CODES -->
 <?php
-    require_once("connect.php");
     if(isset($_POST["save"])){
-        $id=htmlspecialchars($_POST['id']);
-        $name=htmlspecialchars($_POST['name']);
-        $location=htmlspecialchars($_POST['home_Address']);
-        $mobile=htmlspecialchars($_POST['mobile_Number']);
-        $email=htmlspecialchars($_POST['email']);
-        $password=htmlspecialchars(string: $_POST['password']);
+        
+        try{
+            $host = "localhost";
+            $user = "root";
+            $password = "";
+            $dbname = "crud";
+            $connection = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "success";
 
-        // VALIDATE USER INPUT
-        $errors = [];
-        if(empty($id) || !ctype_digit($id)){
-            $errors[] = "Invalid id";
-        }
-        if(empty($name)|| !ctype_alpha($name)){
-            $errors[] = "Name must contain only Alphabets";
-        }
-        if(empty($location)||!ctype_alnum($location)){
-            $errors[]="Invalid input for location";
-        }
-        if(empty($mobile)||!ctype_digit($mobile)){
-            $errors[] = "Invalid Mobile Number";
-        }
-        if(empty($email)||!filter_var($email, FILTER_VALIDATE_EMAIL)){
-            $errors[] = "Invalid E-mail Address!";
-        }
-        if(empty($password)){
-            $errors[] = "Enter a password";
-        } else{
-            $password = password_hash($password, PASSWORD_DEFAULT);
+            $id=filter_var($_POST['form_Number'], FILTER_VALIDATE_INT);
+            $name=filter_var($_POST['full_Name'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $location=filter_var($_POST['home_Address'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $mobile=htmlspecialchars($_POST['mobile_Number']);
+            $email=htmlspecialchars($_POST['email']);
+            $password=htmlspecialchars(string: $_POST['password']);
+
+            // VALIDATE USER INPUT
+            $errors = [];
+            if(empty($id)){
+                $errors[] = "Invalid id";
+            }
+            if(empty($name)){
+                $errors[] = "Enter Full Name";
+            }
+            if(empty($location)||!ctype_alnum($location)){
+                $errors[]="Invalid input for location";
+            }
+            if(empty($mobile)||!ctype_digit($mobile)){
+                $errors[] = "Invalid Mobile Number";
+            }
+            if(empty($email)||!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                $errors[] = "Invalid E-mail Address!";
+            }
+            if(empty($password)){
+                $errors[] = "Enter a password";
+            } else{
+                $password = password_hash($password, PASSWORD_DEFAULT);
+            }
+
+            // CHECK IF THERE ARE ANY ERRORS
+            if(!empty($errors)){
+                echo "Errors:";
+                foreach( $errors as $error){
+                    echo '<br>' . $error;
+                }
+            } else{
+                // PREPARE AND EXECUTE SQL STATEMENT
+                $sql = ("INSERT INTO user_info(ID, Name, Location, Mobile, Email, Password) VALUES(?, ?, ?, ?, ?, ?)");
+                $stmt = $connection->prepare($sql);
+                $stmt->bindParam(1, $id, PDO::PARAM_STR);
+                $stmt->bindParam(2, $name, PDO::PARAM_STR);
+                $stmt->bindParam(3, $location, PDO::PARAM_STR);
+                $stmt->bindParam(4, $mobile, PDO::PARAM_STR);
+                $stmt->bindParam(5, $email, PDO::PARAM_STR);
+                $stmt->bindParam(6, $password, PDO::PARAM_STR);
+                
+                $stmt->execute();
+                
+            }
+        } catch(PDOException $e){
+            echo $e->getMessage();
         }
 
-        // CHECK IF THERE ARE ANY ERRORS
-        if(!empty($errors)){
-            echo "Errors:";
-            foreach( $errors as $error){
-                echo '<br>' . $error;
-            }
-        } else{
-            // PREPARE AND EXECUTE SQL STATEMENT
-            $sql = ("INSERT INTO user_info(ID, Name, Locatioin, Mobile, Email, Password) VALUES(?, ?, ?, ?, ?, ?)");
-            $stmt = $connection->prepare($sql);
-            $stmt->bindParam(1, $id, PDO::PARAM_STR);
-            $stmt->bindParam(2, $name, PDO::PARAM_STR);
-            $stmt->bindParam(3, $location, PDO::PARAM_STR);
-            $stmt->bindParam(4, $mobile, PDO::PARAM_STR);
-            $stmt->bindParam(5, $email, PDO::PARAM_STR);
-            $stmt->bindParam(6, $password, PDO::PARAM_STR);
-
-            // CHECK IF INFO IS INSERTES
-            if($stmt->rowCount()>0){
-                echo "Data inserted successfully";
-            }
-            else{
-                echo "Failed to insert Data";
-            }
-        }
+        
     }
 ?>
